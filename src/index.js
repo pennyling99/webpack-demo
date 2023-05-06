efbec5c4c7c6cec44cabc2042b913f8e480762c6
pennyling99 <38998246+pennyling99@users.noreply.github.com>
2023-05-06 14:52:43 +0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import _ from 'lodash';
+import './style.css';
+import Vue from 'vue';
+
+// document.getElementById('app').innerHTML = 'Hello, webpack lala';
+
+const vm = new Vue({
+  el: '#app',
+  template: `<div id="app">{{messsage}}</div>`,
+  data: {
+    message: 'Hello Vue',
+  },
+});
